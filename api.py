a8e0efa7244e616b6cd9bda75d8dccfa350014fc
Qing-Cheng Li <qc.linux@gmail.com>
2018-03-09 00:21:31 +0100

    try routing

--- api.py
+++ api.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import webapp2
-from webapp2_extras import routes
 import json
 
-class MainPage(webapp2.RequestHandler):
+class APIMainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write('QCurrency is working.')
 
-class LatestExchangeRates(webapp2.RequestHandler):
+class APILatestExchangeRates(webapp2.RequestHandler):
     def get(self):
         rates = {}
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(json.dumps(rates))
 
 app = webapp2.WSGIApplication([
-        routes.PathPrefixRoute('/api', [
-            webapp2.Route('/latest', LatestExchangeRates),
-        ]), 
-        ('/', MainPage),
+        ('/',APIMainPage),
+        ('/api', APILatestExchangeRates),
     ], debug=True)
